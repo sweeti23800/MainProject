22dee9cc601361fc3abe6bc83f65670175837ac6
Gautam-kumar-098 <gk3638386@gmail.com>
2026-02-08 16:32:37 +0530

    Add Project Files

---
+++ routes/listing.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+
+const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
+
+router.route("/").get(wrapAsync(listingController.index)).post(
+  isLoggedIn,
+
+  upload.single("listing[image]"),
+  validateListing,
+  wrapAsync(listingController.createListing),
+);
+
+// search route
+router.get(
+  "/search",
+  isLoggedIn,
+  wrapAsync(async (req, res) => {
+    let { country } = req.query;
+
+    if (!country) {
+      return res.redirect("/listings");
+    }
+
+    let allListings = await Listing.find({
+      country: { $regex: country, $options: "i" },
+    });
+
+    res.render("listings/index", { allListings });
+  }),
+);
+
+//new route
+router.get("/new", isLoggedIn, listingController.renderNewForm);
+
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.updateListing),
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
+
+//edit route
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.editListing),
+);
+
+module.exports = router;
